1186a95e06a49ae600a65b983d1dae04bbf75db8
Tyla Devon <tyladevon@gmail.com>
2020-04-03 21:06:21 -0600

    Refactor User model for pluralization accuracy

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ validates_presence_of :first_name, :last_name, :google_token, :self_care_time
 validates :email, uniqueness: true
 
 has_many :user_activities, dependent: :destroy
-has_many :categorie_activities, through: :user_activities
+has_many :category_activities, through: :user_activities
 end
