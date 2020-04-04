a269d817a883941bf498867eb84483bbab519288
Tyla Devon <tyladevon@gmail.com>
2020-04-03 21:04:47 -0600

    Create Category model spec

---
+++ spec/models/category_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+	describe "Validations" do 
+		it {should validate_presence_of :name}
+	end
+
+	describe "Relationships" do 
+		it {should have_many :category_activities}
+		it {should have_many(:activities).through(:category_activities)}
+	end
+end
