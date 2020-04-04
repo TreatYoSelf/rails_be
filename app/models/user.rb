5b499eeb82333a857ca4b70eb7d61c6835c36f05
Tyla Devon <tyladevon@gmail.com>
2020-04-04 11:25:01 -0600

    Refactor User, take out self care time temporarily

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-validates_presence_of :first_name, :last_name, :google_token, :self_care_time
+validates_presence_of :first_name, :last_name, :google_token
 validates :email, uniqueness: true
 
 has_many :user_activities, dependent: :destroy
