5f32522f4e8a0bb36c73acd64508285869076f8f
Tyla Devon <tyladevon@gmail.com>
2020-04-04 11:24:25 -0600

    Create spec for graphql user find by google token

---
+++ spec/graphql/queries/user/user_google_token_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do 
+	describe 'find user' do 
+		it 'can query a single user by google token' do 
+			user = User.create(first_name: "Sue", last_name: "Buckets", email: "suebuckets@gmail.com", google_token: "018723y4kjd" )
+
+			result = TreatYoSelfSchema.execute(query).as_json
+		end
+		def query
+			<<~GQL
+			{
+				getUserGoogleToken(id: "018723y4kjd")
+				{
+					firstName
+					lastName
+					email
+					googleToken
+				}
+			}
+			GQL
+		end
+	end
+end
