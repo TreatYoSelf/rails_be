67be1a01154b58bf6176b7079df7696461097a5e
Wren Steitle <lauren.steitle@gmail.com>
2020-04-04 17:17:47 -0600

    Update rspec user by google token test to match Tyla's

--- spec/graphql/queries/user/user_google_token_spec.rb
+++ spec/graphql/queries/user/user_google_token_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Types::QueryType do 
-	describe 'find user' do 
-		it 'can query a single user by google token' do 
+RSpec.describe Types::QueryType do
+	describe 'find user' do
+		it 'can query a single user by google token' do
 			user = User.create(first_name: "Sue", last_name: "Buckets", email: "suebuckets@gmail.com", google_token: "018723y4kjd" )
 
 			result = TreatYoSelfSchema.execute(query).as_json
+			expect(result["data"]["getUserGoogleToken"]["firstName"]).to eq("Sue")
 		end
 		def query
 			<<~GQL
