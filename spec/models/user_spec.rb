333dcad0d4c8d1bddcc0e43ec6c5f787394182fb
Tyla Devon <tyladevon@gmail.com>
2020-04-04 16:41:54 -0600

    Remove validation for self care time

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe User, type: :model do
 		it {should validate_presence_of :first_name}
 		it {should validate_presence_of :last_name}
 		it {should validate_presence_of :google_token}
-		it {should validate_presence_of :self_care_time}
 	end
 
 	describe "Relationships" do 
