5c921e71e146431b26564079983655523907f930
Becky Robran <rerobran@gmail.com>
2020-04-04 20:06:06 -0600

    adds user activities to to query fields

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -4,9 +4,17 @@ module Types
       argument :id, ID, required: true
     end
 
+    field :find_user_activities, Types::UserType, null: false, description: "returns user activities" do
+      argument :id, ID, required: true
+    end
+
     # resolvers below/controller actions
     def get_user_google_token(id:)
       User.find_by(google_token: id)
+    end
+
+    def find_user_activities(id:)
+      User.find(id)
     end
   end
 end
