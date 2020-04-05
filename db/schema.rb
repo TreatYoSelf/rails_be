b4d2811cc6e2d22815d3d934348ec66f697c6173
Becky Robran <rerobran@gmail.com>
2020-04-05 16:39:31 -0600

    changes data type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_03_011120) do
+ActiveRecord::Schema.define(version: 2020_04_05_171321) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,9 +49,9 @@ ActiveRecord::Schema.define(version: 2020_04_03_011120) do
     t.string "last_name"
     t.string "email"
     t.string "google_token"
-    t.integer "self_care_time"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.interval "self_care_time"
   end
 
   add_foreign_key "category_activities", "activities"
