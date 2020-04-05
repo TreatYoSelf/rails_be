9f9d21272fdb75d8a99caf14300bf2befaac8009
Becky Robran <rerobran@gmail.com>
2020-04-04 20:06:25 -0600

    adds activities field for user

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -5,5 +5,12 @@ module Types
 		field :last_name, String, null: false
 		field :email, String, null: false
 		field :google_token, String, null: false
+		field :activities, [String], null: true
+
+		def activities
+			object.activities.map do |activity|
+				activity.name
+			end
+		end
 	end
 end
