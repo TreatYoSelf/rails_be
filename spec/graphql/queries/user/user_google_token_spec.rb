7b960e21e1ff0bd9ced0a833ee0190e279d6d61e
Becky Robran <rerobran@gmail.com>
2020-04-04 18:22:02 -0600

    adds testing to graphql

--- spec/graphql/queries/user/user_google_token_spec.rb
+++ spec/graphql/queries/user/user_google_token_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Types::QueryType do
 	describe 'find user' do
 		it 'can query a single user by google token' do
 			user = User.create(first_name: "Sue", last_name: "Buckets", email: "suebuckets@gmail.com", google_token: "018723y4kjd" )
-
 			result = TreatYoSelfSchema.execute(query).as_json
 			expect(result["data"]["getUserGoogleToken"]["firstName"]).to eq("Sue")
 		end
