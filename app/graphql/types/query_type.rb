a5b4cd17f5d51d83e1f7e067df6103a008ffb17b
Becky Robran <rerobran@gmail.com>
2020-04-04 18:22:02 -0600

    adds testing to graphql

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,11 @@
 module Types
   class QueryType < Types::BaseObject
     field :get_user_google_token, Types::UserType, null: false, description: "returns user by google token" do
-      argument :google_token, String, required: true
+      argument :id, ID, required: true
     end
 
     # resolvers below/controller actions
     def get_user_google_token(id:)
-      binding.pry
       User.find_by(google_token: id)
     end
   end
