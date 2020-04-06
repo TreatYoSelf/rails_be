f35ab11169f8eaf3184e1180601ee6f1e6caa92f
Becky Robran <rerobran@gmail.com>
2020-04-06 13:37:14 -0600

    updates attributes and resolver in file
    Co-Authored-By: Tyla Devon <tyladevon@users.noreply.github.com>

---
+++ app/graphql/mutations/users/create_user.rb
@@ -0,0 +1,16 @@
+module Mutations
+  module Users
+    class CreateUser < ::Mutations::BaseMutation
+      argument :first_name, String, required: true
+      argument :last_name, String, required: true
+      argument :email, String, required: true
+      argument :google_token, String, required: true
+
+      type Types::UserType
+
+      def resolve(attributes)
+        User.create(attributes)
+      end
+    end
+  end
+end
