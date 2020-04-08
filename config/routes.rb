f7613035e90bbfba2379947e2f30c2a9cab7351e
Becky Robran <rerobran@gmail.com>
2020-04-08 17:27:57 -0600

    adds a route to the api google controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@ Rails.application.routes.draw do
   end
 
   post "/graphql", to: "graphql#execute"
+
+  namespace :api do
+    namespace :v1 do
+      get '/google_calender', to: 'googles#index'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
