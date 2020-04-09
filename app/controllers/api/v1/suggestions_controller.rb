0ad44ebcf2015a51d8b0e47075b76fb8d7dacf37
Tyla Devon <tyladevon@gmail.com>
2020-04-08 18:20:13 -0600

    Add suggestions method to SuggestionsController

--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::SuggestionsController < ApplicationController 
 	def create
+		render json: SuggestionSerializer.new(suggestions)
+	end
+
+	private
+	def suggestions
+		SuggestionFacade.new(params)
 	end
 end 
