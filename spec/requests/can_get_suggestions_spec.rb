8564d6559f6d3223c1012fc9bb62cbca3f4cab83
Becky Robran <rerobran@gmail.com>
2020-04-12 12:43:06 -0600

    updates test for suggestions controller
    Co-Authored-By: Tyla Devon <tyladevon@gmail.com>

--- spec/requests/can_get_suggestions_spec.rb
+++ spec/requests/can_get_suggestions_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "'/suggestions' endpoint" do
-	it 'can get suggestions based on incoming params for a user' do 
+RSpec.describe "'/suggestions' endpoint", :vcr do
+	it 'can get suggestions based on incoming params for a user' do
+	user = create (:user)
+	category = Category.create(name: "Outdoors")
+
+	activity = Activity.create(name: "Hike", est_time: "01:15")
+	activity_1 = Activity.create(name: "Swimming", est_time: "01:15")
+	category.activities << [activity, activity_1]
+
+	categoryactivities = CategoryActivity.all
+	user.category_activities << categoryactivities
+	#allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 		user_params = {
-			user_id: 1, 
-			user_name: "Tyla",
-			est_time: 90,
 			category: ["outdoors", "mindfulness", "music"]
 		}
 		post "/api/v1/suggestions", params: user_params
@@ -13,6 +20,7 @@ RSpec.describe "'/suggestions' endpoint" do
 		expect(response).to be_successful
 
 		data = JSON.parse(response.body, symbolize_names: true)
+
 		expect(data).to_not be_empty
 	end
 end
