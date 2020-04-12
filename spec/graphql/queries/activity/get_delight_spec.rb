0611f09b8957e9eb21d4a8d572e29d246425ab1a
Wren Steitle <lauren.steitle@gmail.com>
2020-04-12 15:31:31 -0600

    Add additional check that only one category is returned.

--- spec/graphql/queries/activity/get_delight_spec.rb
+++ spec/graphql/queries/activity/get_delight_spec.rb
@@ -11,10 +11,18 @@ RSpec.describe Types::QueryType do
 			joins2 = CategoryActivity.create(category_id: category.id, activity_id: activity2.id)
 			joins3 = CategoryActivity.create(category_id: category.id, activity_id: activity3.id)
 
+			category2 = Category.create(id: 112, name: "Mindfulness")
+			activity_a = Activity.create(name: "Yoga", est_time: 90)
+			activity_b = Activity.create(name: "Journaling", est_time: 180)
+			activity_c = Activity.create(name: "Listen to David Arkenstone", est_time: 30)
+			joins_a = CategoryActivity.create(category_id: category2.id, activity_id: activity_a.id)
+			joins_b = CategoryActivity.create(category_id: category2.id, activity_id: activity_b.id)
+			joins_c = CategoryActivity.create(category_id: category2.id, activity_id: activity_c.id)
 
 
 			result = TreatYoSelfSchema.execute(query).as_json
-      expect(result["data"]["getDelights"].count).to eq(3)
+			expect(result["data"]["getDelights"].count).to eq(3)
+      expect(result["data"]["getDelights"].count).not_to eq(6)
     end
     def query
       <<~GQL
