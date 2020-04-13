fcb789021ea3ed1ff2875c7e2d3a634e21489a91
Becky Robran <rerobran@gmail.com>
2020-04-13 10:43:31 -0600

    adds vcr cassettes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,8 +18,7 @@ VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  config.filter_sensitive_data("") { ENV[''] }
-  config.filter_sensitive_data("") { ENV[''] }
+  config.filter_sensitive_data("GOOGLE_TOKEN") { ENV['GOOGLE_TOKEN'] }
 end
 
 Capybara.register_driver :selenium do |app|
