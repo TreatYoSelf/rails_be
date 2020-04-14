c0e9b5e5fc5d750e371138d132e10c4349ffc711
Becky Robran <rerobran@gmail.com>
2020-04-13 19:57:16 -0600

    updates schema with event scheduler

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_06_164156) do
+ActiveRecord::Schema.define(version: 2020_04_13_234414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,16 @@ ActiveRecord::Schema.define(version: 2020_04_06_164156) do
     t.index ["category_id"], name: "index_category_activities_on_category_id"
   end
 
+  create_table "event_schedules", force: :cascade do |t|
+    t.string "event_name"
+    t.datetime "event_start_time"
+    t.datetime "event_end_time"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_event_schedules_on_user_id"
+  end
+
   create_table "user_activities", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "category_activity_id", null: false
@@ -56,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_04_06_164156) do
 
   add_foreign_key "category_activities", "activities"
   add_foreign_key "category_activities", "categories"
+  add_foreign_key "event_schedules", "users"
   add_foreign_key "user_activities", "category_activities"
   add_foreign_key "user_activities", "users"
 end
