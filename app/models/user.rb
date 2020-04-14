5f4913d2310eaafd2515e1beb29e4d872d83b875
Becky Robran <rerobran@gmail.com>
2020-04-13 19:56:20 -0600

    adds event_scheduler relationship to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 validates_presence_of :first_name, :last_name, :google_token
 validates :email, uniqueness: true
 
+has_many :event_schedules, dependent: :destroy
 has_many :user_activities, dependent: :destroy
 has_many :category_activities, through: :user_activities
 has_many :activities, through: :category_activities
