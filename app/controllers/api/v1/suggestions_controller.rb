a2d7b065cf4c4db24055e25f300fcce42551c106
Becky Robran <rerobran@gmail.com>
2020-04-15 15:36:43 -0600

    debugging

--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::SuggestionsController < ApplicationController
 	def create
+	p params
 		create_user_activities
 		render json: { response: "Successfully created user activities" }
 	end
