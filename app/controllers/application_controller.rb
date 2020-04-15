a07b241f628ca657e3dfeeab5f3171291a412b5b
Becky Robran <rerobran@gmail.com>
2020-04-15 17:39:33 -0600

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::API
 	helper_method :current_user
 
 	def current_user
-		@current_user = User.find(1)
-	#	@current_user ||= User.find(session[:current_user]) if session[:current_user]
+		@current_user ||= User.find(session[:current_user]) if session[:current_user]
 	end
 end
--- app/poros/suggestion_facade.rb
+++ app/poros/suggestion_facade.rb
@@ -7,7 +7,6 @@ class SuggestionFacade
 			EventSchedule.create(event_name: event_params[0],
 												event_start_time: event_params[1],
 												event_end_time: event_params[2],
-												weekday: event_params[3],
 												user_id: @current_user.id)
 	end
 
@@ -18,7 +17,6 @@ class SuggestionFacade
 		summary = event.summary
 		start = event.start.date_time.to_f
 		end_time = event.end.date_time.to_f
-		weekday = event.start.date_time.strftime("%A")
 	end
 
 	def schedule_activities
